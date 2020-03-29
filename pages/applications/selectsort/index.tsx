98622b033f60acf99bdf1269bb33119025280620
yinxulai <yinxulai@hotmail.com>
2020-03-29 19:42:01 +0800

    实现选择排序

--- pages/applications/selectsort/index.tsx
+++ pages/applications/selectsort/index.tsx
@@ -1,2 +1,7 @@
 import noSSR from 'next/dynamic'
-export default noSSR(import('./render') , { ssr: false })
+import { appInfo } from './config'
+import { Layout } from '../../components/layout'
+
+const Render = noSSR(import('./render'), { ssr: false })
+
+export default (props: AppProps) => (<Layout info={appInfo}><Render {...props} /></Layout>)
