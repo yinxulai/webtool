683394d6513fbb11204aa3e7b775f5b8b6cf096a
yinxulai <yinxulai@hotmail.com>
2020-04-03 09:30:20 +0800

    update

--- pages/applications/basephysical/index.tsx
+++ pages/applications/basephysical/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { appInfo } from './config'
-import { Container } from 'react-pitaya'
 import * as physical from './physical'
+import { Container } from 'react-pitaya'
 import { Layout } from '../../components/layout'
 
 export default function render(_: AppProps) {
